0e464038fc53dae7b52a5df3f7b89afe765fb3bf
Ilya on Raspberry <lii291001@gmail.com>
2021-04-09 11:47:43 +0300

    123

--- scripts/main.py
+++ scripts/main.py
@@ -101,12 +101,12 @@ if __name__ == "__main__":
             # timer = datetime.now()
             if keyevent.scancode == 307:
                 altY = True if event.value == 1 else False
-            if keyevent.scancode == 306: # B
+            if keyevent.scancode == 306: # right
                 if event.value == 1:
                     move_driver(board, 'right_driver' , 'right' if not altY else 'left', speed)
                 if event.value == 0:
                    stop_driver(board, 'right_driver')
-            if keyevent.scancode == 304: # X
+            if keyevent.scancode == 304: # left
                 if event.value == 1:
                     move_driver(board, 'left_driver', 'left' if not altY else 'right', speed)
                 if event.value == 0:
